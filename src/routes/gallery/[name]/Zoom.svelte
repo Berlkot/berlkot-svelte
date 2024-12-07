d2e91777703a8a70ba227b3e8332bbf2f1868089
Berlkot <romicheg0600@mail.ru>
2024-12-07 16:40:37 +0300

    I love transforms and every single bug connected with them

--- src/routes/gallery/[name]/Zoom.svelte
+++ src/routes/gallery/[name]/Zoom.svelte
@@ -1,72 +1,82 @@
 <script lang="ts">
-	import { onMount, tick } from 'svelte';
+	import { tick } from 'svelte';
 
 	let { children } = $props();
 	let viewport: HTMLDivElement | undefined = $state();
 	let activated = $state(false);
-	async function toggleZoom(e: MouseEvent & { currentTarget: EventTarget & HTMLDivElement; }) {
-		activated = !activated;
-		if (!activated) {
-			zoom = 1.5;
-			translateX = 0;
-			translateY = 0;
-		} else {
-			await tick();
-			let offsetx = centerx - e.pageX;
-			let offsety = centery - e.pageY;
-            translateX =
-                (Math.min(
-                    (viewport!.clientWidth / 2) * zoom - Math.abs(offsetx),
-                    Math.abs(offsetx * zoom)
-                ) * Math.sign(offsetx)) / zoom;
-            translateY =
-                (Math.min(
-                    (viewport!.clientHeight / 2) * zoom - Math.abs(offsety),
-                    Math.abs(offsety * zoom)
-                ) * Math.sign(offsety)) / zoom;
-		}
-	}
+	let rect: DOMRect | undefined = $state();
+    async function toggleZoom(e: MouseEvent & { currentTarget: EventTarget & HTMLDivElement }) {
+    activated = !activated;
+
+    if (!activated) {
+        zoom = 1.5;
+        translateX = 0;
+        translateY = 0;
+    } else {
+        await tick();
+		rect = viewport!.getBoundingClientRect();
+        centerx = rect.left + rect.width / 2;
+        centery = rect.top + rect.height / 2;
+		updateOffset(e);
+    }
+}
+
+
+
 	let zoom = $state(1.5);
-	let centerx: number;
-	let centery: number;
+	let centerx: number = $state(0);
+	let centery: number = $state(0);
+	let testx = $state(0);
+	let testy = $state(0);
 	let translateX = $state(0);
 	let translateY = $state(0);
-	let image;
-	onMount(() => {
-		const windowWidth = window.innerWidth;
-		const windowHeight = window.innerHeight;
-		centerx = windowWidth / 2;
-		centery = windowHeight / 2;
-	});
 	function wheel(e: WheelEvent & { currentTarget: EventTarget & HTMLDivElement }) {
 		e.preventDefault();
 		if (e.deltaY < 0) {
-			zoom = Math.min(4, zoom + 0.2);
+			zoom = Math.min(2.8, zoom + 0.2);
 		} else {
 			zoom = Math.max(0.8, zoom - 0.2);
 		}
+
 	}
-	function onmousemove(e: MouseEvent & { currentTarget: EventTarget & HTMLDivElement }) {
-		let offsetx = centerx - e.pageX;
-		let offsety = centery - e.pageY;
-        translateX =
-			(Math.min(
-				(viewport!.clientWidth / 2) * zoom - Math.abs(offsetx),
-				Math.abs(offsetx * zoom)
-			) * Math.sign(offsetx)) / zoom;
-		translateY =
-			(Math.min(
-				(viewport!.clientHeight / 2) * zoom - Math.abs(offsety),
-				Math.abs(offsety * zoom)
-			) * Math.sign(offsety)) / zoom;
-	}
+function onmousemove(e: MouseEvent & { currentTarget: EventTarget & HTMLDivElement }) {
+		rect = viewport!.getBoundingClientRect();	
+		updateOffset(e)
+
+}
+function updateOffset(e: MouseEvent & { currentTarget: EventTarget & HTMLDivElement; }) {
+	const signx = Math.sign(centerx - e.pageX);
+        const signy = Math.sign(centery - e.pageY);
+        let offsetx = Math.abs(centerx - e.pageX);
+        let offsety = Math.abs(centery - e.pageY);
+		const maxOffsetX = (rect!.width / 4)
+		const maxOffsetY = (rect!.height / 4)
+
+		if (offsetx >= maxOffsetX && offsetx - maxOffsetX >= offsety - maxOffsetY) {
+			offsety = (offsety / offsetx) * maxOffsetX 
+			offsetx = maxOffsetX; 
+		} else if (offsety > maxOffsetY && offsety - maxOffsetY >= offsetx - maxOffsetX) {
+			offsetx = (offsetx / offsety) * maxOffsetY;
+			offsety = maxOffsetY; 
+		}
+		translateX = offsetx * signx;
+		translateY = offsety * signy;
+}
+
+
+
+
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <!-- svelte-ignore a11y_click_events_have_key_events -->
-<div onclick={async (e) => toggleZoom(e)} bind:this={image}>
+<div onclick={async (e) => toggleZoom(e)}>
 	{@render children?.()}
 </div>
+<div class="test" style:top={`${centery}px`} style:left={`${centerx}px`}></div>
+<div class="test" style:top={`${testy}px`} style:left={`${testx}px`}></div>
+<!-- <div class="test" style:top={rect?.top + "px"} style:left={rect?.left + "px"} style:width={rect?.width + "px"} style:height={rect?.height + "px"}></div> -->
+
 
 {#if activated}
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
@@ -77,8 +87,7 @@
 			class="zoomable"
 			onwheel={(e) => wheel(e)}
 			onmousemove={(e) => onmousemove(e)}
-			style:transform={`scale(${zoom}) translate(${translateX}px, ${translateY}px)`}
-		>
+			style:transform={`translate(${translateX}px, ${translateY}px) scale(${zoom})`} style:transform-origin={`50% 50%`}>
 			{@render children?.()}
 		</div>
 	</div>
@@ -103,5 +112,8 @@
 		overflow: hidden;
 		align-items: center;
 		justify-content: center;
+	}
+	.zoomable {
+		display: flex;
 	}
 </style>
