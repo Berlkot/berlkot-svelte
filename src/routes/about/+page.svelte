72c1bce033a1c05f7a4f04bacf613f7fec215692
Berlkot <romicheg0600@mail.ru>
2025-01-06 23:29:09 +0300

    Fix grammar&punctuation in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -18,36 +18,36 @@
 	<h2>Who</h2>
 	<p class="sub">Berlkot | he/him</p>
 	<p>
-		At least that how i called in the virtual world. This name based on combining word
-		<strong>"berlian"</strong> as for <strong>"shiny"</strong> and word "cat" written in russian transliteration
+		At least that's how I called in the virtual world. This name is based on combining word
+		<strong>"berlian"</strong> as for <strong>"shiny"</strong> and word "cat" written in russian transliteration.
 	</p>
 	<p>
 		I'm also a <strong>CS student</strong> studying how to make machines do *beep-boop*. Made some progress in image
-		processing and game dev
+		processing and game dev.
 	</p>
 	<p>
-		<strong>As an artist</strong> i really like developing an overall style for a project. Have some expiriense
-		working in a team as a background & concept artist
+		<strong>As an artist,</strong> I really like developing an overall style for a project. Have some experience
+		working in a team as a background & concept artist.
 	</p>
 	<p>
-		<strong>In free time</strong>, i really like tinkering with stuff (thats why i have linux as my main system) and gaming. And i also somehow got myself involved into furry fandom. Now I really want to make my own fursuit at some point <span class="accent">:D</span>
+		<strong>In free time</strong>, I really like tinkering with stuff (that's why I have linux as my main system) and gaming. And I also somehow got myself involved into furry fandom. Now I really want to make my own fursuit at some point <span class="accent">:D</span>
 	</p>
 </section>
 <section>
 	<h2>Website</h2>
 	<p>
-		This website was build through pain and grind during session period as mean to make myself more
-		familiar with web development in general. It has its own CMS and other neat stuff build from
+		This website was built through pain and grind during a session period as a mean to make myself more
+		familiar with web development in general. It has its own CMS and other neat stuff, built from the
 		ground up.
 	</p>
 	<p>
-		I'm <strong>still working</strong> on some major parts of it. For example RSS and sitemap are still missing. And the projects tab... But i have something sweat planned as a way to test my web animation framework. So keep an eye on that <span class="accent">0w0</span>
+		I'm <strong>still working</strong> on some major parts of it. For example, RSS and sitemap are still missing. And the projects tab... But I have something sweet planned as a way to test my web animation framework. So keep an eye on that <span class="accent">0w0</span>
 	</p>
 </section>
 <section>
 	<h2>Contact</h2>
 	<p>
-		Right now my social media are kinda stalling. But I have plans in the future to return to updating them
+		Right now, my social media are kinda stalling. But I have plans in the future to return to updating them.
 	</p>
 	<div class="social">
 		<ul>
